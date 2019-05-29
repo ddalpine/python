f9683ba6f283f7255201c007a55c9d83b692b385
ddalpine <50346654+ddalpine@users.noreply.github.com>
2019-05-30 06:34:14 +0800

    Revert "no"
    This reverts commit a547eb6a103a6084e0d54980935d834598fd646e.

--- 100题/lianxi10.py
+++
@@ -1,5 +0,0 @@
-#coding=utf-8
-
-print("\1\1")
-for i in range(2,8):
-	print("*"*i)
--- 100题/lianxi13.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-i =100
-while i<1000:
-	b = int(i//100)
-	s = int(i/10%10)
-	g = int(i%100%10)
-	if b**3+s**3+g**3==i:
-		print(i)
-	i+=1
